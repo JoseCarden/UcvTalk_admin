24911feb951a3ba33ca91f40676ce7999da6c475
GersonZeta <gezetab@ucvvirtual.edu.pe>
2024-05-24 11:08:38 -0500

    Graficos de Reporte Estudiante
    El dato Asunto de la tabla reporte_estudiante de la BD se refleja en el grafico pie

--- src/app/pages/califica-profes/califica-profes.page.ts
+++ src/app/pages/califica-profes/califica-profes.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import Chart from 'chart.js/auto';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-califica-profes',
@@ -7,9 +9,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalificaProfesPage implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getDataAndCreatePieChart();
   }
-  
+
+  getDataAndCreatePieChart() {
+    this.http.get<any[]>('http://localhost:3000/repor-estudiante').subscribe(data => {
+      const asuntos = data.map(item => item.Asunto);
+      const counts = this.countOccurrences(asuntos);
+      this.createPieChart(counts);
+    });
+  }
+
+  countOccurrences(arr: any[]): { [key: string]: number } {
+    return arr.reduce((acc, val) => {
+      acc[val] = acc[val] ? acc[val] + 1 : 1;
+      return acc;
+    }, {});
+  }
+
+  createPieChart(counts: { [key: string]: number }) {
+    const labels = Object.keys(counts);
+    const data = Object.values(counts);
+
+    const ctx = document.getElementById('myPieChart') as HTMLCanvasElement;
+    const myPieChart = new Chart(ctx, {
+      type: 'pie',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'Asuntos',
+          data: data,
+          backgroundColor: [
+            'red',
+            'blue',
+            'yellow',
+            'green',
+            'purple',
+            'orange'
+          ],
+          hoverOffset: 4
+        }]
+      }
+    });
+  }
 }
